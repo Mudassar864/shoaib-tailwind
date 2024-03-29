fc570415bac06df748ced02a651838b820eccc43
mudassar864 <mohsanbhatti864@gmail.com>
2024-03-29 12:44:12 +0500

    loader

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
-export default function Loading() {
+export default function loading() {
   return (
-    <div>
-      Loading...
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "3rem",
+      }}
+    >
+      Shaoib<span style={{ fontWeight: "700" ,marginLeft:"0.5rem"}}> Shahid.</span>
     </div>
-  )
+  );
 }

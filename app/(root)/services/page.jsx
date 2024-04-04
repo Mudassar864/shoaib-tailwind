637c495462b641101344c8fe5178b73c6ba77f00
mudassar864 <mohsanbhatti864@gmail.com>
2024-04-04 20:50:06 +0500

    update

--- app/(root)/services/page.jsx
+++ app/(root)/services/page.jsx
@@ -19,10 +19,10 @@ export default function page() {
         />
       </div>
       <div className="max-w-3xl mx-auto my-16 flex flex-col gap-4">
-        <h2 className="font-medium text-2xl text-center tracking-widest text-sothebys-blue">
+        <h2 className="text-4xl text-center  text-sothebys-blue font-bold">
           SPACIOUS RETREATS
         </h2>
-        <p className="text-sm text-center font-normal">
+        <p className="text-base text-center font-normal font-benton">
           Gaze across the uninterrupted horizon from your private water villa,
           suspended over the ocean. Discover the iconic city skylines surrounded
           by tranquil waterways. Enjoy your front-row seat to the wonders of
@@ -34,8 +34,8 @@ export default function page() {
       <div className="py-16">
         <div className="max-w-5xl  mx-auto  flex  ">
           <div className="basis-[60%] p-16 bg-sothebys-blue text-white relative after:content-[''] after:absolute after:-right-36 after:-z-10 after:top-0 after:w-36 after:h-full after:bg-sothebys-blue">
-            <h2 className="text-2xl mb-4">EXCLUSIVELY YOURS</h2>
-            <p className="font-benton font-normal">
+            <h2 className="text-4xl mb-4 font-medium">EXCLUSIVELY YOURS</h2>
+            <p className="font-benton font-normal text-base">
               Nowhere else do the wonders of the planet’s most incredible
               locations fuse so intimately with the warm feeling of home.
               Spacious retreats that envelop you in seclusion and seamlessly

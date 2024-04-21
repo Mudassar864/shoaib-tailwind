b6ee56344a4c7d0da48c02cc2d5d5bd265655203
Mudassar Manzoor <44868262+Mudassar864@users.noreply.github.com>
2024-04-21 11:59:29 +0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,20 +6,20 @@ import NextTopLoader from "nextjs-toploader";
 import { GoogleAnalytics } from '@next/third-parties/google'
 
 export const metadata = {
-  title: "Shoaib Shahid - Global Real Estate Consultant",
+  title: "Shoaib Shahid - International Real Estate Management Consultant",
   description:
-    "Shoaib Shahid is a seasoned global real estate consultant with over 15 years of experience. He has overseen property sales worth $1.5 billion, specializing in off-plan properties, luxury real estate, and prestigious locations.",
+    "Shoaib Shahid is a international real estate management consultant with over 15 years of experience. He has overseen property sales worth $1.5 billion, specializing in off-plan properties, luxury real estate, and prestigious locations.",
   // Social Media Meta Tags
-  ogTitle: "Shoaib Shahid - Global Real Estate Consultant",
+  ogTitle: "Shoaib Shahid - International Real Estate Management Consultant",
   ogDescription:
-    "Shoaib Shahid is a seasoned global real estate consultant with over 15 years of experience. He has overseen property sales worth $1.5 billion, specializing in off-plan properties, luxury real estate, and prestigious locations.",
+    "Shoaib Shahid is a international real estate management consultant with over 15 years of experience. He has overseen property sales worth $1.5 billion, specializing in off-plan properties, luxury real estate, and prestigious locations.",
   ogType: "website",
   ogUrl: "https://www.theshoaibshahid.com/", // Replace with actual URL
   ogImage:
     "https://shoaib-tailwind.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fabout.d3bd4028.jpg&w=1920&q=75", // Replace with actual image URL
   twitterTitle: "Shoaib Shahid - Global Real Estate Consultant",
   twitterDescription:
-    "Shoaib Shahid is a seasoned global real estate consultant with over 15 years of experience. He has overseen property sales worth $1.5 billion, specializing in off-plan properties, luxury real estate, and prestigious locations.",
+    "Shoaib Shahid is a international real estate management consultant with over 15 years of experience. He has overseen property sales worth $1.5 billion, specializing in off-plan properties, luxury real estate, and prestigious locations.",
   twitterCard: "summary_large_image",
   twitterImage:
     "https://shoaib-tailwind.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fabout.d3bd4028.jpg&w=1920&q=75", // Replace with actual image URL
